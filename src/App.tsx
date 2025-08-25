ec34f746dc61a8f04d1f49c609dd17007c8f9fda
Ishant Agarwal <ishantagarwal32@gmail.com>
2025-08-25 14:58:48 +0700

    files updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BoardProvider } from "./contexts/BoardContext/BoardContext";
 import { KanbanBoard } from "./components/pages/KanbanBoard/KanbanBoard";
+import { Analytics } from "@vercel/analytics/react";
 
 export const App: React.FC = () => (
   <BoardProvider>
     <KanbanBoard />
+    <Analytics />
   </BoardProvider>
 );
